10ffcf6ed2be8366786d2e50de2794aa5f83def6
Theis Nielsen Haahr <Theishaahr@yahoo.dk>
2024-04-16 15:59:04 +0200

    Slight changes

--- src/components/BenefitsSection.vue
+++ src/components/BenefitsSection.vue
@@ -3,17 +3,17 @@
         <h2>Benefits</h2>
         <div class="benefits-wrapper">
             <div class="benefit-item">
-                <img src="../assets/Moisturizing.png" alt="">
+                <img src="../assets/Moisturizing.png" alt="Moisturizing icon">
                 <h3>Moisturizing</h3>
                 <p>Helps keep your skin clean and moisturised throughout the day.</p>
             </div>
             <div class="benefit-item">
-                <img src="../assets/100p natural.png" alt="">
+                <img src="../assets/100p natural.png" alt="100% natural icon">
                 <h3>100% Natural</h3>
                 <p>Made from 100% natural ingredients. No chemicals added.</p>
             </div>
             <div class="benefit-item">
-                <img src="../assets/Cruelty free.png" alt="">
+                <img src="../assets/Cruelty free.png" alt="Cruelty free icon">
                 <h3>Cruelty-free</h3>
                 <p>No animal or human was hurt in the making of our products.</p>
             </div>
