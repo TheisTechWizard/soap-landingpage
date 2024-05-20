7adca1c1c5cfb542c23f43520a190b845657bda4
Theis Nielsen Haahr <Theishaahr@yahoo.dk>
2024-05-20 15:55:03 +0200

    About me section updates

--- src/components/CreatorSection.vue
+++ src/components/CreatorSection.vue
@@ -4,14 +4,26 @@
             <h2>Meet the creator</h2>
             <div class="content">
                 <p>Hi! My name is Theis Nielsen Haahr</p>
+                <p>I am</p>
                 <p>If you like what you've seen, and want to talk about future cooperation opportunities, or just want
                     to say hello, feel free to reach out to me! Here's how you can get in touch with me.</p>
                 <p>Email: <a href="mailto:theishaahr@gmail.com">theishaahr@gmail.com</a></p>
                 <p>Phone: <a href="tel:+4571708705">71708705</a></p>
+                <p>You are also welcome to connect with me, and hit me up on linkedIn. And if you are curious, the
+                    source code for this website is available on my GitHub profile.
+                </p>
+                <div class="social-media-links ">
+                    <a href="https://www.linkedin.com/in/theishaahr/">
+                        <img src="..\assets\LinkedIn Logo.png" alt="LinkedIn Logo">
+                    </a>
+                    <a href="https://github.com/TheisTechWizard/soap-landingpage">
+                        <img src="..\assets\GitHub Logo.png" alt="GitHub Logo" />
+                    </a>
+                </div>
             </div>
         </div>
         <div class="right-side-content">
-            <img src="" alt="">
+            <img src="../assets/Creator-TheisHaahr.png" alt="Picture of Theis Haahr">
         </div>
     </section>
 </template>
